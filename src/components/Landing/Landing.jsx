be2e68053f3c79db238d5843cdc98e626fc4d04e
DClark <duncan.clark528@gmail.com>
2025-09-30 16:40:59 -0400

    basic searchbar

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,20 +1,42 @@
-import { useEffect,useState,useContext } from "react";
+import { useEffect, useState, useContext } from "react";
 
 import { UserContext } from "../../contexts/UserContext";
 
-import * as userService from "../../services/userService"
+import * as userService from "../../services/userService";
 
 const Landing = ()=> {
 
     const {user} = useContext(UserContext)
 
-    const [addresses,setAddress] = useState([])
+    const [addresses, setAddress] = useState([]);
+
+    const [searchQuery, setSearchQuery] = useState('');
+
+
+const handleChange = (evt) => {
+    setSearchQuery(evt.target.value);
+    console.log(evt.target.value)
+    // onSearch(evt.target.value);
+};
+
+const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log('This has been submitted');
+}
 
 
     return (
         <>
             {!user? (<aside>Please login to see your saved addresses!</aside>):
             (<aside>Welcome {user.username}</aside>)}
+        
+            <form onSubmit={handleSubmit}>
+                <input type="text"
+                placeholder="Search Addresses"
+                value={searchQuery}
+                onChange={handleChange}/>
+                <button>Submit</button>
+            </form>
         </>
     )
 }
