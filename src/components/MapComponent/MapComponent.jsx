eea93aaa1bf73bc4f974ff159e4bc6e2fcb135ad
Jonathan <jonathanallen105@gmail.com>
2025-10-01 12:10:40 -0400

    removed testing code.

--- src/components/MapComponent/MapComponent.jsx
+++ src/components/MapComponent/MapComponent.jsx
@@ -1,18 +1,11 @@
 import { APIProvider, Map} from "@vis.gl/react-google-maps"
-import * as googleMapService from '../../services/googleMapsService'
 
 const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
 
 const MapComponent = () => {
 
-    const fetchCords = async () => {
-        const cords = await googleMapService.getGeocode('5 Crystal Springs Road, Greenville, SC')
-        console.log(cords)
-    }
-
     return ( 
         <APIProvider apiKey={apiKey} >
-            <button onClick={fetchCords}>get cords</button>
             <Map
                 style={{width: '100vw', height: '100vh'}}
                 defaultCenter={{lat: 22.54992, lng: 0}}
