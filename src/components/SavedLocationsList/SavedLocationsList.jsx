c2129b67edad0d296047fe452aca5a5316da98a1
Rtkmak14 <Rtkmak14@gmail.com>
2025-10-01 14:42:54 -0500

    use effect update

--- src/components/SavedLocationsList/SavedLocationsList.jsx
+++ src/components/SavedLocationsList/SavedLocationsList.jsx
@@ -1,20 +1,33 @@
+import { Link } from "react-router";
+import { useContext } from "react";
+import { UserContext } from "../../contexts/UserContext";
+
+
 function SavedLocationsList({ savedLocations, handleEditSavedLocations, handleDeleteSavedLocations }) {
+  const {user} = useContext(UserContext)
+
+  console.log(savedLocations)
+  if (!savedLocations.locations) {
+    return (
+      <div>
+        Loading...
+      </div>
+    )
+  }
+
   return (
     <div className="saved-locations">
       <h2>Saved Addresses</h2>
       <ul>
-        {savedLocations.map((savedLocation) => (
+        {savedLocations.locations.map((savedLocation) => (
           <li key={savedLocation._id}>
-            <p>
-              {savedLocation.name}
-            </p>
-            <div>
-              <button onClick={() => handleEditSavedLocations(savedLocation)}>Edit</button>
-              <button onClick={() => handleDeleteSavedLocations(savedLocation._id)}>Delete</button>
-            </div>
+              <Link to={`/users/${user._id}/${savedLocation._id}`}>
+                  {savedLocation.name}
+              </Link>
           </li>
         ))}
       </ul>
+      <Link to="/locations/new">Add New</Link>
     </div>
   );
 }
