55d8c42768f1ae052450728d8fb6a30e48c7aa94
DClark <duncan.clark528@gmail.com>
2025-10-02 10:15:16 -0400

    local main commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,17 @@ import SignInForm from './components/signInForm/signInForm'
 import Landing from './components/Landing/Landing'
 import LocationForm from "./components/SavedAddressessForm/SavedAddressessForm";
 import MapComponent from "./components/MapComponent/MapComponent";
+import { useNavigate } from "react-router";
 
 
 const App = () => {
 
-const [selectedSavedLocation,setselectedSavedLocation] = useState(null)
+const navigate = useNavigate();//added
+const [selectedSavedLocation, setselectedSavedLocation] = useState(null)
 
 const handleEdit = (location)=> {
    setselectedSavedLocation(location)
+   navigate('/locations/new') //added passed to route locations/new
 }
 
   return (
@@ -24,7 +27,7 @@ const handleEdit = (location)=> {
         <Route path="/" element={<Landing handleEdit={handleEdit}/>}/> {/* confirm if this needs to be conditional*/ }
         <Route path='/sign-up' element={<SignUpForm />} />
         <Route path='/sign-in' element={<SignInForm />} />
-        <Route path='/locations/new' element={<LocationForm/> }/>
+        <Route path='/locations/new' element={<LocationForm selectedSavedLocation={selectedSavedLocation} />}/>
       </Routes>
     </>
   )
